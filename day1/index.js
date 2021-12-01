70c277ec19f1f3aaedccf2bae70aade609687818
Nathan Richards <nld@logios.co.uk>
2021-12-01 08:12:19 +0000

    Cleanup and refactor day1

--- day1/index.js
+++ day1/index.js
@@ -1,16 +1,6 @@
 const fs = require("fs");
 
-let raw = fs.readFileSync("input.txt", "utf8").toString();
-// raw = `199
-// 200
-// 208
-// 210
-// 200
-// 207
-// 240
-// 269
-// 260
-// 263`;
+const raw = fs.readFileSync("input.txt", "utf8").toString();
 const depths = raw.trim().split("\n").map(Number);
 
 function part1(depths) {
@@ -24,10 +14,8 @@ function part1(depths) {
 function part2(depths) {
     let increasing = 0;
     let previousWindow = depths[0] + depths[1] + depths[2];
-    for(let i=1; i<depths.length - 2; i++) {
-        const currentWindow = depths[i] + depths[i+1] + depths[i+2];
-        //console.log(`Previous window = ${previousWindow}`);
-        //console.log(`Current window = ${currentWindow}`);
+    for(let i=3; i<depths.length; i++) {
+        const currentWindow = depths[i] + depths[i-1] + depths[i-2];
         if (currentWindow > previousWindow) {
             increasing++;
         }
@@ -37,7 +25,4 @@ function part2(depths) {
 }
 
 console.log(part1(depths));
-
 console.log(part2(depths));
-
-// 1702 too low
