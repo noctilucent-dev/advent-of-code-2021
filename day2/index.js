ec121d03c1156b19eff30204bbc3de8741ca206d
Nathan Richards <nld@logios.co.uk>
2021-12-02 08:40:56 +0000

    Solved day 2

---
+++ day2/index.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+
+let raw = fs.readFileSync("input.txt", "utf8").toString();
+// raw = `forward 5
+// down 5
+// forward 8
+// up 3
+// down 8
+// forward 2`;
+
+const instructions = raw.trim().split("\n");
+
+function part1(instructions) {
+    let depth = 0;
+    let horizontal = 0;
+
+    for(let i=0; i<instructions.length; i++) {
+        let [c, v] = instructions[i].split(" ");
+        v = Number(v);
+        if (c === "forward") {
+            horizontal += v;
+        } else if (c === "down") {
+            depth += v;
+        } else if (c === "up") {
+            depth -= v;
+        } else {
+            throw new Error(`Unrecognised instruction ${c}`);
+        }
+    }
+
+    return horizontal * depth;
+}
+
+function part2(instructions) {
+    let depth = 0;
+    let horizontal = 0;
+    let aim = 0;
+
+    for(let i=0; i<instructions.length; i++) {
+        let [c, v] = instructions[i].split(" ");
+        v = Number(v);
+        if (c === "forward") {
+            horizontal += v;
+            depth += aim * v;
+        } else if (c === "down") {
+            aim += v;
+        } else if (c === "up") {
+            aim -= v;
+        } else {
+            throw new Error(`Unrecognised instruction ${c}`);
+        }
+    }
+
+    return horizontal * depth;
+}
+
+console.log(part1(instructions));
+console.log(part2(instructions));
