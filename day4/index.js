87add26ab1eccb80a2a14ec5dff19139f0e99296
Nathan Richards <nld@logios.co.uk>
2021-12-04 19:46:13 +0000

    Cleaned up day 4

--- day4/index.js
+++ day4/index.js
@@ -2,28 +2,6 @@ const fs = require("fs");
 
 let raw = fs.readFileSync("input.txt", "utf8").toString();
 
-// raw = `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
-
-// 22 13 17 11  0
-//  8  2 23  4 24
-// 21  9 14 16  7
-//  6 10  3 18  5
-//  1 12 20 15 19
-
-//  3 15  0  2 22
-//  9 18 13 17  5
-// 19  8  7 25 23
-// 20 11 10 24  4
-// 14 21 16 12  6
-
-// 14 21 17 24  4
-// 10 16 15  9 19
-// 18  8 23 26 20
-// 22 11 13  6  5
-//  2  0 12  3  7`;
-
-
-
 class Board {
     constructor(lines) {
         this.rows = lines.map(l => l.trim().split(/\s+/));
@@ -67,22 +45,15 @@ class Board {
     }
 }
 
-function parseBoard(lines) {
-    return new Board(lines);
+function parseBoards(lines) {
+    let boards = [];
+
+    for (let i=2; i<lines.length; i+=6) {
+        boards.push(new Board(lines.slice(i, i + 5)));
+    }
+
+    return boards;
 }
-
-const lines = raw.trim().split("\n");
-
-const draws = lines[0].split(",");
-
-let boards = [];
-
-for (let i=2; i<lines.length; i+=6) {
-    boards.push(parseBoard(lines.slice(i, i + 5)));
-}
-
-console.log(draws);
-console.log(boards);
 
 function part1(draws, boards) {
     let winner;
@@ -90,7 +61,8 @@ function part1(draws, boards) {
         boards.forEach(b => b.mark(draws[i]));
         winner = boards.find(b => b.bingo());
         if (winner) {
-            console.log(winner);
+            console.log("First winner:")
+            console.log(winner.rows);
             return winner.score(draws[i]);
         }
     }
@@ -103,12 +75,17 @@ function part2(draws, boards) {
         boards.forEach(b => b.mark(draws[i]));
         boards = boards.filter(b => !b.bingo());
         if (last && boards.length === 0) {
-            console.log(last);
+            console.log("Last winner:")
+            console.log(last.rows);
             return last.score(draws[i]);
         }
     }
 }
 
-console.log(part2(draws, boards));
+const lines = raw.trim().split("\n");
+const draws = lines[0].split(",");
 
-// part 2 15960 too high
+const p1 = part1(draws, parseBoards(lines));
+const p2 = part2(draws, parseBoards(lines));
+console.log(`Part 1: ${p1}`);
+console.log(`Part 2: ${p2}`);
