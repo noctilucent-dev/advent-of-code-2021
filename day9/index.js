6f5bd34f75087f96e9daac6de690228f0ce6f392
Nathan Richards <nld@logios.co.uk>
2021-12-09 22:41:20 +0000

    Solved day 9 part 2

--- day9/index.js
+++ day9/index.js
@@ -6,7 +6,8 @@ let raw = fs.readFileSync("input.txt", "utf8").toString();
 let DEBUG = !!process.env.DEBUG;
 
 if (DEBUG) {
-    raw = `2199943210
+    raw = `
+    2199943210
     3987894921
     9856789892
     8767896789
@@ -39,18 +40,78 @@ function getAdjacent(x, y, heightMap) {
     return adjacentCoords;
 }
 
-function part1(heightMap) {
+function findLowPoints(heightMap) {
     const lowPoints = [];
     for (let y=0; y<heightMap.length; y++) {
         for (let x=0; x<heightMap[0].length; x++) {
             const adjacentCoords = getAdjacent(x, y, heightMap);
             const anyLower = adjacentCoords.find(([ax, ay]) => heightMap[ay][ax] <= heightMap[y][x]);
             if (!anyLower) {
-                lowPoints.push(heightMap[y][x]);
+                lowPoints.push([x, y]);
             }
         }
     }
-    return lowPoints.reduce((p, c) => p + c + 1, 0);
+    return lowPoints;
+}
+
+function findBasin(x, y, heightMap) {
+    log(`Finding basin [${x},${y}]`);
+    heightMap = [...heightMap.map(l => [...l])];
+    heightMap[y][x] = -1;
+    while (true) {
+        log(heightMap);
+        let growing = false;
+        for (let y=0; y<heightMap.length; y++) {
+            for (let x=0; x < heightMap[y].length; x++) {
+                if (heightMap[y][x] > -1) continue;
+
+                // left
+                if (x > 0 && heightMap[y][x-1] > -1 && heightMap[y][x-1] < 9) {
+                    heightMap[y][x-1] = -1;
+                    growing = true;
+                }
+
+                // right
+                if (x < heightMap[y].length - 1 && heightMap[y][x+1] > -1 && heightMap[y][x+1] < 9) {
+                    heightMap[y][x+1] = -1;
+                    growing = true;
+                }
+
+                // up
+                if (y > 0 && heightMap[y-1][x] > -1 && heightMap[y-1][x] < 9) {
+                    heightMap[y-1][x] = -1;
+                    growing = true;
+                }
+
+                // down
+                if (y < heightMap.length - 1 && heightMap[y+1][x] > -1 && heightMap[y+1][x] < 9) {
+                    heightMap[y+1][x] = -1;
+                    growing = true;
+                }
+            }
+        }
+        if (!growing) {
+            break;
+        }
+    }
+
+    log(heightMap);
+    const size = heightMap.reduce((p, row) => p + row.filter(v => v === -1).length, 0);
+    log(`Size ${size}`);
+
+    return size;
+}
+
+function part1(heightMap) {
+    const lowPoints = findLowPoints(heightMap);
+    return lowPoints.reduce((p, [x, y]) => p + heightMap[y][x] + 1, 0);
+}
+
+function part2(heightMap) {
+    const lowPoints = findLowPoints(heightMap);
+    const sizes = lowPoints.map(([x, y]) => findBasin(x,y,heightMap));
+    sizes.sort((a, b) => b - a);
+    return sizes.slice(0,3).reduce((p, c) => p * c, 1);
 }
 
 const heightMap = raw.trim().split("\n").map(l => l.trim().split("").map(Number));
@@ -58,3 +119,4 @@ const heightMap = raw.trim().split("\n").map(l => l.trim().split("").map(Number)
 log(heightMap);
 
 console.log(part1(heightMap));
+console.log(part2(heightMap));
