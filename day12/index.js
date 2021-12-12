5913e5b5e004b8f88eae6ae903083001e4281495
Nathan <52113097+noctilucent-dev@users.noreply.github.com>
2021-12-12 21:32:22 +0000

    Solved day 12 part 2

--- day12/index.js
+++ day12/index.js
@@ -28,28 +28,63 @@ function isBigCave(name) {
     return name.toUpperCase() === name;
 }
 
-function recurseSearch(graph, path) {
-    const last = path[path.length-1];
-    if (last === "end") {
-        log(path);
-        return [path];
-    }
-
-    const childPaths = graph[last]
-        .filter(c => isBigCave(c) || !path.includes(c))
-        .map(c => recurseSearch(graph, [...path,c]))
-        .flat();
-
-    return childPaths;
-}
-
-function part1(graph) {
-    const allPaths = recurseSearch(graph, ["start"]);
-    log(allPaths);
-    return allPaths.length;
-}
-
 const graph = parseGraph(raw.trim().split("\n"));
 log(graph);
 
-console.log(part1(graph));
+(function() {
+    function recurseSearch(graph, path) {
+        const last = path[path.length-1];
+        if (last === "end") {
+            log(path);
+            return [path];
+        }
+
+        const childPaths = graph[last]
+            .filter(c => isBigCave(c) || !path.includes(c))
+            .map(c => recurseSearch(graph, [...path,c]))
+            .flat();
+
+        return childPaths;
+    }
+
+    function part1(graph) {
+        const allPaths = recurseSearch(graph, ["start"]);
+        log(allPaths);
+        return allPaths.length;
+    }
+
+    console.log(part1(graph));
+})();
+
+(function() {
+    function recurseSearch(graph, path) {
+        const last = path[path.length-1];
+        if (last === "end") {
+            log(path);
+            return [path];
+        }
+
+        const childPaths = graph[last]
+            .filter(c => {
+                if (c === "start") return false;
+                if (isBigCave(c)) return true;
+                if (!path.includes(c)) return true;
+                if (path.filter((item, index) => !isBigCave(item) && path.indexOf(item) !== index).length === 0) {
+                    return true;
+                }
+                return false;
+            })
+            .map(c => recurseSearch(graph, [...path,c]))
+            .flat();
+
+        return childPaths;
+    }
+
+    function part2(graph) {
+        const allPaths = recurseSearch(graph, ["start"]);
+        log(allPaths);
+        return allPaths.length;
+    }
+
+    console.log(part2(graph));
+})();
